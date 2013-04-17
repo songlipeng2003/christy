5f7c89837da8d5ec62a5eb6e89ffc575e4185698
slm1949 <songliming1949@gmail.com>
2013-04-17 20:17:18 +0800

    修改布局和后台主页，增加分类管理、出版社管理、添加图书的链接

--- protected/modules/admin/views/default/index.php
+++ protected/modules/admin/views/default/index.php
@@ -2,9 +2,28 @@
 /* @var $this DefaultController */
 
 $this->breadcrumbs=array(
-	$this->module->id,
+$this->module->id,
 );
 ?>
+<p>
+	<a href="<?php echo $this->module->id; ?>/add" class="btn btn-primary">添加书籍</a>
+</p>
+<table class="table">
+    <tr>
+        <td>ID</td>
+        <td>书名</td>
+        <td>作者</td>
+        <td>分类</td>
+        <td>出版社</td>
+        <td>ISBN</td>
+        <td>备注</td>
+        <td>创建时间</td>
+        <td>更新时间</td>
+        <td>操作</td>
+    </tr>
+</table>
+
+<!--
 <h1><?php echo $this->uniqueId . '/' . $this->action->id; ?></h1>
 
 <p>
@@ -15,3 +34,4 @@ in the "<?php echo $this->module->id; ?>" module.
 <p>
 You may customize this page by editing <tt><?php echo __FILE__; ?></tt>
 </p>
+-->
