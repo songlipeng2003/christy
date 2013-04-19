ae51fb4aa9abef6308d20b75c0228710f2468b70
Thinking Song <123wuming_123@163.com>
2013-04-20 00:00:35 +0800

    修改后台生产的controller所有都需要admin权限

--- protected/extensions/bootstrap/gii/bootstrap/templates/admin/controller.php
+++ protected/extensions/bootstrap/gii/bootstrap/templates/admin/controller.php
@@ -35,11 +35,11 @@ class <?php echo $this->controllerClass; ?> extends <?php echo $this->baseContro
 		return array(
 			array('allow',  // allow all users to perform 'index' and 'view' actions
 				'actions'=>array('index','view'),
-				'users'=>array('*'),
+				'users'=>array('admin'),
 			),
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
 				'actions'=>array('create','update'),
-				'users'=>array('@'),
+				'users'=>array('admin'),
 			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
 				'actions'=>array('admin','delete'),
