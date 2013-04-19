847702a259776ac94fc395a79cfcc8af7be469b5
Thinking Song <123wuming_123@163.com>
2013-04-20 00:04:52 +0800

    修后台操作显示

--- protected/modules/admin/views/layouts/column2.php
+++ protected/modules/admin/views/layouts/column2.php
@@ -1,24 +1,15 @@
 <?php /* @var $this Controller */ ?>
 <?php $this->beginContent('/layouts/main'); ?>
 <div class="row">
-    <div class="span9">
+    <div class="span12">
+        <?php 
+        foreach($this->menu as $menu){
+            $this->widget('bootstrap.widgets.TbButton', $menu);
+        }
+        ?>
         <div id="content">
             <?php echo $content; ?>
         </div><!-- content -->
-    </div>
-    <div class="span3">
-        <div id="sidebar">
-        <?php
-            $this->beginWidget('zii.widgets.CPortlet', array(
-                'title'=>'操作',
-            ));
-            $this->widget('bootstrap.widgets.TbMenu', array(
-                'items'=>$this->menu,
-                'htmlOptions'=>array('class'=>'operations'),
-            ));
-            $this->endWidget();
-        ?>
-        </div><!-- sidebar -->
     </div>
 </div>
 <?php $this->endContent(); ?>
