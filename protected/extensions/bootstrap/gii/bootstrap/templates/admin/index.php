9eb25e08f97b135674ecb8932397ea4781c7a23f
Thinking Song <123wuming_123@163.com>
2013-04-19 23:56:37 +0800

    添加后台的admin的gii生成模版

---
+++ protected/extensions/bootstrap/gii/bootstrap/templates/admin/index.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * The following variables are available in this template:
+ * - $this: the BootCrudCode object
+ */
+?>
+<?php
+echo "<?php\n";
+$label=$this->pluralize($this->class2name($this->modelClass));
+echo "\$this->breadcrumbs=array(
+	'$label'=>array('index'),
+	'管理',
+);\n";
+?>
+
+$this->menu=array(
+	array('label'=>'创建 <?php echo $this->modelClass; ?>','url'=>array('create')),
+);
+
+Yii::app()->clientScript->registerScript('search', "
+$('.search-button').click(function(){
+	$('.search-form').toggle();
+	return false;
+});
+$('.search-form form').submit(function(){
+	$.fn.yiiGridView.update('<?php echo $this->class2id($this->modelClass); ?>-grid', {
+		data: $(this).serialize()
+	});
+	return false;
+});
+");
+?>
+
+<h1>管理 <?php echo $this->pluralize($this->class2name($this->modelClass)); ?></h1>
+
+<p>
+你可以选择输入一个比较运算符 (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>
+or <b>=</b>) 在每个搜索值前来指定如何应该做比较.
+</p>
+
+<?php echo "<?php echo CHtml::link('高级搜索','#',array('class'=>'search-button btn')); ?>"; ?>
+
+<div class="search-form" style="display:none">
+<?php echo "<?php \$this->renderPartial('_search',array(
+	'model'=>\$model,
+)); ?>\n"; ?>
+</div><!-- search-form -->
+
+<?php echo "<?php"; ?> $this->widget('bootstrap.widgets.TbGridView',array(
+	'id'=>'<?php echo $this->class2id($this->modelClass); ?>-grid',
+	'dataProvider'=>$model->search(),
+	'filter'=>$model,
+	'columns'=>array(
+<?php
+$count=0;
+foreach($this->tableSchema->columns as $column)
+{
+	if(++$count==7)
+		echo "\t\t/*\n";
+	echo "\t\t'".$column->name."',\n";
+}
+if($count>=7)
+	echo "\t\t*/\n";
+?>
+		array(
+			'class'=>'bootstrap.widgets.TbButtonColumn',
+		),
+	),
+)); ?>
---
+++ protected/extensions/bootstrap/gii/bootstrap/templates/admin/view.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The following variables are available in this template:
+ * - $this: the BootCrudCode object
+ */
+?>
+<?php
+echo "<?php\n";
+$nameColumn=$this->guessNameColumn($this->tableSchema->columns);
+$label=$this->pluralize($this->class2name($this->modelClass));
+echo "\$this->breadcrumbs=array(
+	'$label'=>array('index'),
+	\$model->{$nameColumn},
+);\n";
+?>
+
+$this->menu=array(
+	array('label'=>'Update <?php echo $this->modelClass; ?>','url'=>array('update','id'=>$model-><?php echo $this->tableSchema->primaryKey; ?>)),
+	array('label'=>'Delete <?php echo $this->modelClass; ?>','url'=>'#','linkOptions'=>array('submit'=>array('delete','id'=>$model-><?php echo $this->tableSchema->primaryKey; ?>),'confirm'=>'你确定删除吗?')),
+	array('label'=>'Manage <?php echo $this->modelClass; ?>','url'=>array('admin')),
+);
+?>
+
+<h1>View <?php echo $this->modelClass." #<?php echo \$model->{$this->tableSchema->primaryKey}; ?>"; ?></h1>
+
+<?php echo "<?php"; ?> $this->widget('bootstrap.widgets.TbDetailView',array(
+	'data'=>$model,
+	'attributes'=>array(
+<?php
+foreach($this->tableSchema->columns as $column)
+	echo "\t\t'".$column->name."',\n";
+?>
+	),
+)); ?>
