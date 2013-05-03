027bd09f8d151fbd873480d61a0ff174d7adc2a9
slm1949 <songliming1949@gmail.com>
2013-05-03 19:46:19 +0800

    布局里添加flash的信息提示的代码

--- themes/bootstrap/views/layouts/column2.php
+++ themes/bootstrap/views/layouts/column2.php
@@ -3,6 +3,17 @@
 <div class="row">
     <div class="span9">
         <div id="content">
+            <?php
+                $this->widget('bootstrap.widgets.TbAlert', array(
+                    'block'=>true, // display a larger alert block?
+                    'fade'=>true, // use transitions?
+                    'closeText'=>'&times;', // close link text - if set to false, no close link is displayed
+                    'alerts'=>array( // configurations per alert type
+                        'success'=>array('block'=>true, 'fade'=>true, 'closeText'=>'&times;'), 
+                        'error'=>array('block'=>true, 'fade'=>true, 'closeText'=>'&times;'), 
+                    ),
+                ));
+            ?>
             <?php echo $content; ?>
         </div><!-- content -->
     </div>
