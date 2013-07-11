096f75af2fff97ef387da490d39d6bd073c7b36c
Thinking Song <123wuming_123@163.com>
2013-07-11 21:06:50 +0800

    修改网站名称为佳美之处

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,7 @@ Yii::setPathOfAlias('bootstrap', dirname(__FILE__).'/../extensions/bootstrap');
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
+	'name'=>'佳美之处',
 	'theme'=>'bootstrap',
 	'language'=>'zh_cn',
 
