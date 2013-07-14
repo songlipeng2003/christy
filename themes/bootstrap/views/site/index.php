4b105f9952a8d154d8359119e79540b92015e1b8
Thinking Song <123wuming_123@163.com>
2013-07-15 00:17:28 +0800

    修改首页为欢迎页面

--- themes/bootstrap/views/site/index.php
+++ themes/bootstrap/views/site/index.php
@@ -1,41 +1,21 @@
-<div class="container-fluid">
-      <div class="row-fluid">
-        <div class="span3">
-          <div class="well sidebar-nav">
-            <ul class="nav nav-list">
-              <li class="nav-header">分类</li>
-              <?php $categories=CHtml::listData(Category::model()->findAll(), 'id' , 'name');
-                foreach ($categories as $key => $value) {
-                  echo '<li><a href="book/Category/'.$key.'">'.$value.'</a></li>';
-                }
-             ?>
-            </ul>
-          </div><!--/.well -->
-        </div><!--/span-->
-
-        <div class="span9">
-          
-          <div class="row-fluid">
-              <?php $books=Book::model()->findAll();
-                foreach ($books as $key => $value) {
-                  echo '<div class="span3">';
-                  echo '<h2>'.$value->name.'</h2>';
-                  echo '<p><a href="book/id/'.$value->id.'"> <img src="upload/'.$value->picture.'" class="img-rounded"> </a> </p>';
-                  echo '</div><!--/span-->';
-                }
-             ?>
-          </div><!--/row-->
-        </div><!--/span-->
-      </div><!--/row-->
-
-<p>You may change the content of this page by modifying the following two files:</p>
-
-<ul>
-    <li>View file: <code><?php echo __FILE__; ?></code></li>
-    <li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
-</ul>
-
-<p>For more details on how to further develop this application, please read
-    the <a href="http://www.yiiframework.com/doc/">documentation</a>.
-    Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
-    should you have any questions.</p>
+<?php $this->beginWidget('bootstrap.widgets.TbHeroUnit', array(
+    'heading'=>'欢迎来到佳美之处！',
+)); ?>
+ 
+    <p>欢迎弟兄姊妹，来到佳美之处基督徒社区，愿为弟兄姊妹提供一个开发、信仰纯正的交流平台。</p>
+    <p>
+    <?php $this->widget('bootstrap.widgets.TbButton', array(
+        'type'=>'primary',
+        'size'=>'large',
+        'label'=>'加入网站',
+        'url'=>array('user/register')
+    )); ?>
+    <?php $this->widget('bootstrap.widgets.TbButton', array(
+        'type'=>'primary',
+        'size'=>'large',
+        'label'=>'登陆',
+        'url'=>array('site/login')
+    )); ?>
+    </p>
+ 
+<?php $this->endWidget(); ?>
