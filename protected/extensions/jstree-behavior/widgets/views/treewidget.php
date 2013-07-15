f2af7fdfd6518e8509997bba1e68ee5c0b56b869
Thinking Song <123wuming_123@163.com>
2013-07-15 12:53:31 +0800

    添加jstree-behavior扩展

---
+++ protected/extensions/jstree-behavior/widgets/views/treewidget.php
@@ -0,0 +1,8 @@
+<div class="row">
+    <input id="reload" type="button" value="刷新" class="btn btn-large pull-left">
+    <input id="add_root" type="button" value="创建一级分类" class="btn btn-large pull-left">
+</div>
+<div class="row">
+    <!--The tree will be rendered in this div-->
+    <div class="well" style="margin-top: 20px" class="row" id="<?php echo $this->jstree_container_ID;?>"></div>
+</div>
