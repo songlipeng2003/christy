b709e1ab8138b3ffe3ad32c50188e5f9c3e5d43c
Thinking Song <123wuming_123@163.com>
2013-07-25 00:44:56 +0800

    前端模版加入错误信息显示

--- themes/bootstrap/views/layouts/main.php
+++ themes/bootstrap/views/layouts/main.php
@@ -46,6 +46,18 @@
 		)); ?><!-- breadcrumbs -->
 	<?php endif?>
 
+    <?php
+        $this->widget('bootstrap.widgets.TbAlert', array(
+            'block'=>true, // display a larger alert block?
+            'fade'=>true, // use transitions?
+            'closeText'=>'&times;', // close link text - if set to false, no close link is displayed
+            'alerts'=>array( // configurations per alert type
+                'success'=>array('block'=>true, 'fade'=>true, 'closeText'=>'&times;'), 
+                'error'=>array('block'=>true, 'fade'=>true, 'closeText'=>'&times;'), 
+            ),
+        ));
+    ?>
+
 	<?php echo $content; ?>
 
 	<div class="clear"></div>
