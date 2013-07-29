867f2d4ce87b7a90736bd51b1e831f5f86345971
Thinking Song <123wuming_123@163.com>
2013-07-29 09:47:38 +0800

    添加群组成员 CRUD #62

---
+++ protected/models/Member.php
@@ -0,0 +1,96 @@
+<?php
+
+/**
+ * This is the model class for table "member".
+ *
+ * The followings are the available columns in table 'member':
+ * @property integer $id
+ * @property integer $user_id
+ * @property integer $group_id
+ * @property string $created_at
+ * @property string $updated_at
+ */
+class Member extends CActiveRecord
+{
+	/**
+	 * Returns the static model of the specified AR class.
+	 * @param string $className active record class name.
+	 * @return Member the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'member';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('user_id, group_id', 'required'),
+			array('user_id, group_id', 'numerical', 'integerOnly'=>true),
+			array('created_at, updated_at', 'safe'),
+			// The following rule is used by search().
+			// Please remove those attributes that should not be searched.
+			array('id, user_id, group_id, created_at, updated_at', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		return array(
+			'user'=>array(self::BELONGS_TO, 'User', 'user_id'),
+			'group'=>array(self::BELONGS_TO, 'Group', 'group_id')
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'id' => Yii::t('model', 'Member.id'),
+			'user_id' => Yii::t('model', 'Member.user'),
+			'group_id' => Yii::t('model', 'Member.group'),
+			'created_at' => Yii::t('model', 'Member.created_at'),
+			'updated_at' => Yii::t('model', 'Member.updated_at'),
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// Warning: Please modify the following code to remove attributes that
+		// should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('id',$this->id);
+		$criteria->compare('user_id',$this->user_id);
+		$criteria->compare('group_id',$this->group_id);
+		$criteria->compare('created_at',$this->created_at,true);
+		$criteria->compare('updated_at',$this->updated_at,true);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+}
