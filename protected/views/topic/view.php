69e3cf4307a0a1296aa589799707a4df13bdbbd5
Thinking Song <123wuming_123@163.com>
2013-08-03 22:32:25 +0800

    帖子页显示论坛信息

--- protected/views/topic/view.php
+++ protected/views/topic/view.php
@@ -7,8 +7,19 @@ $this->breadcrumbs=array(
 	$topic->title,
 );
 ?>
-<h1><?php echo $topic->title ?></h1>
+<div class="row">
+    <div class="span8">
+        <h1><?php echo $topic->title ?></h1>
 
-<div class="">
-    <?php echo $topic->content ?>
+        <div class="">
+            <?php echo $topic->content ?>
+        </div>
+    </div>
+    <div class="span4">
+        <div class="well">
+            <h1><?php echo $topic->group->name; ?></h1>
+            <p>创建于:<?php echo Yii::app()->dateFormatter->formatDateTime($topic->group->created_at, 'medium', false); ?> 组长:<?php echo $topic->group->user->username ?></p>
+            <p><?php echo $topic->group->summary ?></p>
+        </div>
+    </div>
 </div>
