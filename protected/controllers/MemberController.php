0cf0f53811f194724529aa2ce3485c1b2ba0c9ca
Thinking Song <123wuming_123@163.com>
2013-08-27 00:28:12 +0800

    修改小组成员权限

--- protected/controllers/MemberController.php
+++ protected/controllers/MemberController.php
@@ -2,16 +2,45 @@
 
 class MemberController extends Controller
 {
+
+    /**
+     * @return array action filters
+     */
+    public function filters()
+    {
+        return array(
+            'accessControl', // perform access control for CRUD operations
+        );
+    }
+
+    /**
+     * Specifies the access control rules.
+     * This method is used by the 'accessControl' filter.
+     * @return array access control rules
+     */
+    public function accessRules()
+    {
+        return array(
+            array(
+                'allow',
+                'actions'=>array('create','delete'),
+                'users'=>array('@'),
+            ),
+            array(
+                'deny',
+                'users'=>array('*')
+            )
+        );
+    }
+
     public function actionCreate()
     {
-        $group_id = $_GET['group_id'];
-        if(!$_GET['group_id'])
-        {
+        if(!isset($_GET['group_id'])){
             throw new CHttpException(404,'The requested page does not exist.');
         }
+        $group_id = $_GET['group_id'];
         $group = Group::model()->findByPk($group_id);
-        if(!$group_id)
-        {
+        if(!$group){
             throw new CHttpException(404,'The requested page does not exist.');
         }
 
@@ -27,8 +56,7 @@ class MemberController extends Controller
             $member = new Member;
             $member->attributes=$attrs;
 
-            if($member->save())
-            {
+            if($member->save()){
                 Yii::app()->user->setFlash('success', '加入成功');
             }else{
                 Yii::app()->user->setFlash('error', '加入失败');
@@ -40,14 +68,12 @@ class MemberController extends Controller
 
     public function actionDelete()
     {
-        $group_id = $_GET['group_id'];
-        if(!$_GET['group_id'])
-        {
+        if(!isset($_GET['group_id'])){
             throw new CHttpException(404,'The requested page does not exist.');
         }
+        $group_id = $_GET['group_id'];
         $group = Group::model()->findByPk($group_id);
-        if(!$group_id)
-        {
+        if(!$group){
             throw new CHttpException(404,'The requested page does not exist.');
         }
 
@@ -58,8 +84,7 @@ class MemberController extends Controller
         $member = Member::model()->findByAttributes($attrs);
 
         if($member){
-            if($member->delete())
-            {
+            if($member->delete()){
                 Yii::app()->user->setFlash('success', '退出成功');
             }else{
                 Yii::app()->user->setFlash('error', '退出失败');
